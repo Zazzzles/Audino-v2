33d34958f1f074cc2746a8d78fefe6e4767f0204
Ras <rasbrits@gmail.com>
2022-08-15 15:35:28 +0200

    did build

---
+++ docs/precache-manifest.d0fd2e3a0a53de1bea82dcfd9d100dad.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "debdb8b9b3d1b835e947e1a2e5dd8d61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2ece3c93a73d174c604",
+    "url": "/static/css/main.a99a77ed.chunk.css"
+  },
+  {
+    "revision": "757392d19d1ae74c58d5",
+    "url": "/static/js/2.17b86d33.chunk.js"
+  },
+  {
+    "revision": "a2ece3c93a73d174c604",
+    "url": "/static/js/main.3f407033.chunk.js"
+  },
+  {
+    "revision": "7c394c61eb6f354e3e47",
+    "url": "/static/js/runtime-main.ae312267.js"
+  },
+  {
+    "revision": "f1a18cf12b1119bc81ddeddea329d66f",
+    "url": "/static/media/absa.f1a18cf1.png"
+  },
+  {
+    "revision": "c252829b0db705f3c062ad7557227bc1",
+    "url": "/static/media/example.c252829b.csv"
+  },
+  {
+    "revision": "cb74ed57c7d6b985c81046b5b1cc2d60",
+    "url": "/static/media/fnb.cb74ed57.png"
+  },
+  {
+    "revision": "dcbc397471e3684f2e5286a0d6d5a30b",
+    "url": "/static/media/landing-bg.dcbc3974.png"
+  },
+  {
+    "revision": "04e2ba5e33130abae7aff7a9497152f5",
+    "url": "/static/media/logo.04e2ba5e.png"
+  },
+  {
+    "revision": "fbff1ee735968fb00ce765e1ecc04d0c",
+    "url": "/static/media/nedbank.fbff1ee7.png"
+  },
+  {
+    "revision": "b2cbe806f5d762332642e24ef823735e",
+    "url": "/static/media/uc.b2cbe806.svg"
+  }
+]);
